3c342ee5513b2c0c523b693ce2808834ab546d59
Wudrou <73528461+Wudrou@users.noreply.github.com>
2020-11-10 10:45:44 +0300

    Добавьте файлы проекта.

---
+++ L1.1_Greetings/Greetings.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1._1_Greetings
+{
+    class Greetings
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Пожалуйста, введите своё имя");
+            string myName = Console.ReadLine();
+            Console.WriteLine("Привет {0}", myName);  //также можно "Привет " + myName
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ L1.4_Divider/DivideIt.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L1._4_Divider
+{
+    class DivideIt
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите первое целое число");
+                string temp = Console.ReadLine();
+                int firstInt = Int32.Parse(temp);
+
+                Console.WriteLine("Введите второе целое число");
+                string temp2 = Console.ReadLine();
+                int secondInt = Int32.Parse(temp2);
+
+                int result = firstInt / secondInt;
+                Console.WriteLine("Результат деления {0} на {1} = {2}", firstInt, secondInt, result);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Исключение: " + e);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
