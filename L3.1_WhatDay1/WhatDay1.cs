690c0b1bff92c4db88308007f737ae3c594eb5d1
Wudrou <73528461+Wudrou@users.noreply.github.com>
2020-11-12 11:03:01 +0300

    Change switch to перечисление

--- L3.1_WhatDay1/WhatDay1.cs
+++ L3.1_WhatDay1/WhatDay1.cs
@@ -8,11 +8,21 @@ namespace L3._1_WhatDay1
 {
     class WhatDay1
     {
-        //enum MonthName
-        //{
-        //    January, February, March, April, May, June, July, August, September, October, November, December
-        //
-        //}
+        enum MonthName
+        {
+            January,
+            February,
+            March,
+            April,
+            May,
+            June,
+            July,
+            August,
+            September,
+            October,
+            November,
+            December
+        }
         static void Main(string[] args)
         {
             Console.Write("Please enter a day number between 1 and 365: ");
@@ -142,37 +152,40 @@ namespace L3._1_WhatDay1
             }
 
         End:
+            
+            MonthName temp = (MonthName)monthNum;
+            string monthName = temp.ToString();
 
-            string monthName;
-            switch (monthNum)
-            {
-                case 0:
-                    monthName = "January"; break;
-                case 1:
-                    monthName = "February"; break;
-                case 2:
-                    monthName = "March"; break;
-                case 3:
-                    monthName = "April"; break;
-                case 4:
-                    monthName = "May"; break;
-                case 5:
-                    monthName = "June"; break;
-                case 6:
-                    monthName = "July"; break;
-                case 7:
-                    monthName = "August"; break;
-                case 8:
-                    monthName = "September"; break;
-                case 9:
-                    monthName = "October"; break;
-                case 10:
-                    monthName = "November"; break;
-                case 11:
-                    monthName = "December"; break;
-                default:
-                    monthName = "not done yet"; break;
-            }
+            //string monthName;
+            //switch (monthNum)
+            //{
+            //    case 0:
+            //        monthName = "January"; break;
+            //    case 1:
+            //        monthName = "February"; break;
+            //    case 2:
+            //        monthName = "March"; break;
+            //    case 3:
+            //        monthName = "April"; break;
+            //    case 4:
+            //        monthName = "May"; break;
+            //    case 5:
+            //        monthName = "June"; break;
+            //    case 6:
+            //        monthName = "July"; break;
+            //    case 7:
+            //        monthName = "August"; break;
+            //    case 8:
+            //        monthName = "September"; break;
+            //    case 9:
+            //        monthName = "October"; break;
+            //    case 10:
+            //        monthName = "November"; break;
+            //    case 11:
+            //        monthName = "December"; break;
+            //    default:
+            //        monthName = "not done yet"; break;
+            //}
 
             Console.WriteLine("{0} {1}", dayNum, monthName);
 
