26303c1cd8101a319f8d9967b1c7927a5d0daa5c
Wudrou <73528461+Wudrou@users.noreply.github.com>
2020-11-12 11:41:09 +0300

    Change //12 if to foreach

--- L3.1_WhatDay1/WhatDay1.cs
+++ L3.1_WhatDay1/WhatDay1.cs
@@ -31,130 +31,145 @@ namespace L3._1_WhatDay1
 
             int monthNum = 0;
 
-            if (dayNum <= 31)  //January
+            List<int> DaysInMonths = new List<int>() {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
+            foreach (int daysInMonth in DaysInMonths)
             {
-                goto End;
-            }
-            else
-            {
-                dayNum = dayNum - 31;
-                monthNum++;
+                if (dayNum <= daysInMonth)
+                {
+                    break;
+                }
+                else
+                {
+                    dayNum = dayNum -daysInMonth;
+                    monthNum++;
+                }
             }
 
-            if (dayNum <= 28)  //February
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum = dayNum - 28;
-                monthNum++;
-            }
-
-            if (dayNum <= 31)  //March
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum = dayNum - 31;
-                monthNum++;
-            }
-
-            if (dayNum <= 30)  //April
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum = dayNum - 30;
-                monthNum++;
-            }
-
-            if (dayNum <= 31)  //May
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum = dayNum - 31;
-                monthNum++;
-            }
-
-            if (dayNum <= 30)  //June
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum = dayNum - 30;
-                monthNum++;
-            }
-
-            if (dayNum <= 31)  //July
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum = dayNum - 31;
-                monthNum++;
-            }
-
-            if (dayNum <= 31)  //August
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum = dayNum - 31;
-                monthNum++;
-            }
-
-            if (dayNum <= 30)  //September
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum = dayNum - 30;
-                monthNum++;
-            }
-
-            if (dayNum <= 31)  //October
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum = dayNum - 31;
-                monthNum++;
-            }
-
-            if (dayNum <= 30)  //November
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum = dayNum - 30;
-                monthNum++;
-            }
-
-            if (dayNum <= 31)  //December
-            {
-                goto End;
-            }
-            else
-            {
-                dayNum = dayNum - 31;
-                monthNum++;
-            }
-
-        End:
-            
             MonthName temp = (MonthName)monthNum;
             string monthName = temp.ToString();
+
+            //if (dayNum <= 31)  //January
+            //{
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum = dayNum - 31;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 28)  //February
+            //{
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum = dayNum - 28;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 31)  //March
+            //{
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum = dayNum - 31;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 30)  //April
+            //{
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum = dayNum - 30;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 31)  //May
+            //{
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum = dayNum - 31;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 30)  //June
+            //{
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum = dayNum - 30;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 31)  //July
+            //{
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum = dayNum - 31;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 31)  //August
+            //{
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum = dayNum - 31;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 30)  //September
+            //{
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum = dayNum - 30;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 31)  //October
+            //{
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum = dayNum - 31;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 30)  //November
+            //{
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum = dayNum - 30;
+            //    monthNum++;
+            //}
+
+            //if (dayNum <= 31)  //December
+            //{
+            //    goto End;
+            //}
+            //else
+            //{
+            //    dayNum = dayNum - 31;
+            //    monthNum++;
+            //}
+
+            //End:
 
             //string monthName;
             //switch (monthNum)
