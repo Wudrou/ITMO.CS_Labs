e48542a908cfc701ed0a8bb4e0275b4e27cfaa9e
Wudrou <73528461+Wudrou@users.noreply.github.com>
2020-11-13 20:59:49 +0300

    Add L4

---
+++ L4_Utils/Utils.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L4_Utils
+{
+    class Utils
+    {
+        static int Greater(int a, int b)
+        {
+            if (a > b)
+            {
+                return a;
+            }
+            else
+            {
+                return b;
+            }
+        }
+        static void Swap(ref int a, ref int b)
+        {
+            int temp = a;
+            a = b;
+            b = temp;
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number:");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number:");
+            int y = int.Parse(Console.ReadLine());
+
+            int greater = Greater(x, y);
+            Console.WriteLine("The greater value is " + greater);
+
+            Console.WriteLine("Before swap: " + x + "," + y);
+            Swap(ref x, ref y);
+            Console.WriteLine("After swap: " + x + "," + y);
+
+            Console.ReadKey();
+        }
+    }
+}
