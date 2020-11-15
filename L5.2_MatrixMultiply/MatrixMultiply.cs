1e53b8af7363e069e1c0ca212373a8e1a0d835af
Wudrou <73528461+Wudrou@users.noreply.github.com>
2020-11-15 17:55:53 +0300

    Add OutPut, Multiply

--- L5.2_MatrixMultiply/MatrixMultiply.cs
+++ L5.2_MatrixMultiply/MatrixMultiply.cs
@@ -8,24 +8,34 @@ namespace L5._2_MatrixMultiply
 {
     class MatrixMultiply
     {
-        static void Main(string[] args)
+        static void OutPut(int[,] result)
         {
-            int[,] a = new int[2, 2] { { 1, 2 }, { 3, 4 } };
-            //a[0, 0] = 1; a[0, 1] = 2;
-            //a[1, 0] = 3; a[1, 1] = 4;
-
-            int[,] b = new int[2, 2] { { 5, 6 }, { 7, 8 } };
-            //b[0, 0] = 5; b[0, 1] = 6;
-            //b[1, 0] = 7; b[1, 1] = 8;
-
+            for(int r = 0; r < result.GetLength(0); r++)
+            {
+                for (int c = 0; c < result.GetLength(1); c++)
+                {
+                    Console.Write("{0} ", result[r, c]);
+                }
+                Console.WriteLine();
+            }
+        }
+        static int[,] Multiply(int[,] a, int[,] b)
+        {
             int[,] result = new int[2, 2];
             result[0, 0] = a[0, 0] * b[0, 0] + a[0, 1] * b[1, 0];
             result[0, 1] = a[0, 0] * b[0, 1] + a[0, 1] * b[1, 1];
             result[1, 0] = a[1, 0] * b[0, 0] + a[1, 1] * b[1, 0];
             result[1, 1] = a[1, 0] * b[0, 1] + a[1, 1] * b[1, 1];
+            return result;
+        }
 
-            Console.WriteLine(result[0, 0] + "   " + result[0, 1]);
-            Console.WriteLine(result[1, 0] + "   " + result[1, 1]);
+        static void Main(string[] args)
+        {
+            int[,] a = new int[2, 2] { { 1, 2 }, { 3, 4 } };
+            int[,] b = new int[2, 2] { { 5, 6 }, { 7, 8 } };
+
+            int[,] result = Multiply(a, b);
+            OutPut(result);
 
             Console.ReadKey();
         }
