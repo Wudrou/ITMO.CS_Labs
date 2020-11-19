c79b0329c6b695d323d76b4268c0530410b6034a
Wudrou <73528461+Wudrou@users.noreply.github.com>
2020-11-19 15:00:12 +0300

    Add 7.1

---
+++ L7.1_BankAccount/CreateAccount.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L7._1_BankAccount
+{
+    class CreateAccount
+    {
+        enum AccountType
+        {
+            Checking,
+            Deposit
+        }
+        class CreateAccount
+        {
+            static void Main(string[] args)
+            {
+                BankAccount berts = NewBankAccount();
+                Write(berts);
+                TestDeposit(berts);
+                Write(berts);
+                TestWithdraw(berts);
+                Write(berts);
+
+                BankAccount freds = NewBankAccount();
+                Write(freds);
+                TestDeposit(freds);
+                Write(freds);
+                TestWithdraw(freds);
+                Write(freds);
+
+                Console.ReadKey();
+            }
+            static BankAccount NewBankAccount()
+            {
+                BankAccount created = new BankAccount();
+
+                Console.Write("Enter the account balance! : ");
+                decimal balance = decimal.Parse(Console.ReadLine());
+
+                created.Populate(balance);
+
+                return created;
+            }
+            public static void TestDeposit(BankAccount acc)
+            {
+                Console.Write("Enter amount to deposit: ");
+                decimal amount = decimal.Parse(Console.ReadLine());
+                acc.Deposit(amount);
+            }
+            public static void TestWithdraw(BankAccount acc)
+            {
+                Console.Write("Enter amount to withdraw: ");
+                decimal amount = decimal.Parse(Console.ReadLine());
+                if (!acc.Withdraw(amount))
+                {
+                    Console.WriteLine("Insufficient funds.");
+                }
+            }
+            static void Write(BankAccount toWrite)
+            {
+                Console.WriteLine("Account number is {0}", toWrite.Number());
+                Console.WriteLine("Account balance is {0}", toWrite.Balance());
+                Console.WriteLine("Account type is {0}", toWrite.Type());
+            }
+        }
+    }
+}
