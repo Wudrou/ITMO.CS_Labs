a09b12e050416eeef29c6db79aa250f0f9b4be57
Wudrou <73528461+Wudrou@users.noreply.github.com>
2020-11-19 15:27:34 +0300

    Add L7.2

---
+++ L7.2_UtilsReverse/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L7._2_UtilsReverse
+{
+    class Utils
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
