77aadd9393cb882affc6c0bb3b0f9d034cfcf04a
Wudrou <73528461+Wudrou@users.noreply.github.com>
2020-11-19 16:55:04 +0300

    Add L7.5

---
+++ L7.5_TestDisplay/TestDisplay.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L7._5_TestDisplay
+{
+    class TestDisplay
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
