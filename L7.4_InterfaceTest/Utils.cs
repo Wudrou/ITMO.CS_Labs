88edbcd07ffe45122559d55b5fe27304396bdeb3
Wudrou <73528461+Wudrou@users.noreply.github.com>
2020-11-19 16:50:11 +0300

    Add code

--- L7.4_InterfaceTest/Utils.cs
+++ L7.4_InterfaceTest/Utils.cs
@@ -8,9 +8,24 @@ namespace L7._4_InterfaceTest
 {
     class Utils
     {
-        static void Main(string[] args)
+        public static bool IsItFormattable(object x)
         {
+            if (x is IFormattable)
+                return true;
+            else
+                return false;
+        }
+        static void Main()
+        {
+            int i = 0;
+            ulong ul = 0;
+            string s = "Test";
 
+            Console.WriteLine("int: {0}", IsItFormattable(i));
+            Console.WriteLine("ulong: {0}", IsItFormattable(ul));
+            Console.WriteLine("String: {0}", IsItFormattable(s));
+
+            Console.ReadKey();
         }
     }
 }
