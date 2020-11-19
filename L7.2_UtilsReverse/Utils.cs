3d70f6652ad3f539d77ee6620a0bfedec4e2509a
Wudrou <73528461+Wudrou@users.noreply.github.com>
2020-11-19 15:46:25 +0300

    All in one project

--- L7.2_UtilsReverse/Utils.cs
+++ L7.2_UtilsReverse/Utils.cs
@@ -18,83 +18,21 @@ namespace L7._2_UtilsReverse
             }
             s = sRev;
         }
-        //static int Greater(int a, int b)
-        //{
-        //    if (a > b)
-        //    {
-        //        return a;
-        //    }
-        //    else
-        //    {
-        //        return b;
-        //    }
-        //    // Alternative version - more terse
-        //    // return (a>b) > (a) : (b);
-        //}
-        //static void Swap(ref int a, ref int b)
-        //{
-        //    int temp = a;
-        //    a = b;
-        //    b = temp;
-        //}
-        //static bool Factorial(int n, out int answer)
-        //{
-        //    int k;
-        //    int f;
-        //    bool ok = true;
-        //    if (n < 0)
-        //        ok = false;
-        //    try
-        //    {
-        //        checked
-        //        {
-        //            f = 1;
-        //            for (k = 2; k <= n; ++k)
-        //            {
-        //                f = f * k;
-        //            }
-        //        }
-        //    }
-        //    catch (Exception)
-        //    {
-        //        f = 0;
-        //        ok = false;
-        //    }
-        //    answer = f;
-        //    return ok;
-        //}
-        //static void Main(string[] args)
-        //{
-        //    Console.WriteLine("Enter first number:");
-        //    int x = int.Parse(Console.ReadLine());
-        //    Console.WriteLine("Enter second number:");
-        //    int y = int.Parse(Console.ReadLine());
+        static void Main()
+        {
+            string message;
 
-        //    //Using Greater
-        //    int greater = Greater(x, y);
-        //    Console.WriteLine("The greater value is " + greater);
+            // Get an input string
+            Console.WriteLine("Enter string to reverse:");
+            message = Console.ReadLine();
 
-        //    //Using Swap
-        //    Console.WriteLine("Before swap: " + x + "," + y);
-        //    Swap(ref x, ref y);
-        //    Console.WriteLine("After swap: " + x + "," + y);
+            // Reverse the string
+            Reverse(ref message);
 
-        //    //Using Factorial
-        //    int f;
-        //    bool ok;
-        //    Console.WriteLine("Number for factorial:");
-        //    x = int.Parse(Console.ReadLine());
-        //    ok = Factorial(x, out f);
-        //    if (ok)
-        //    {
-        //        Console.WriteLine("Factorial(" + x + ") = " + f);
-        //    }
-        //    else
-        //    {
-        //        Console.WriteLine("Cannot compute this factorial");
-        //    }
+            // Display the result
+            Console.WriteLine(message);
 
-        //    Console.ReadKey();
-        //}
+            Console.ReadLine();
+        }
     }
 }
