688da4cf8f27422f82d73fcfb2ff6483ada124f7
Wudrou <73528461+Wudrou@users.noreply.github.com>
2020-11-19 16:33:45 +0300

    Add L7.4

---
+++ L7.4_InterfaceTest/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L7._4_InterfaceTest
+{
+    class Utils
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
