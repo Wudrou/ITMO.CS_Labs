b566d4674807d371cdf3de3a77849b15e96a7412
Wudrou <73528461+Wudrou@users.noreply.github.com>
2020-11-19 17:50:36 +0300

    Add code 8.1

---
+++ L8.1-2_Constructors/BankAccount.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L8._1_2_Constructors
+{
+    enum AccountType
+    {
+        Checking,
+        Deposit
+    }
+    class BankAccount
+    {
+        private long accNo;
+        private decimal accBal;
+        private AccountType accType;
+        private static long nextAccNo = 123;
+        public BankAccount()
+        {
+            accNo = NextNumber();
+            accType = AccountType.Checking;
+            accBal = 0;
+        }
+        public BankAccount(AccountType aType)
+        {
+            accNo = NextNumber();
+            accType = aType;
+            accBal = 0;
+        }
+
+        public BankAccount(decimal aBal)
+        {
+            accNo = NextNumber();
+            accType = AccountType.Checking;
+            accBal = aBal;
+        }
+
+        public BankAccount(AccountType aType, decimal aBal)
+        {
+            accNo = NextNumber();
+            accType = aType;
+            accBal = aBal;
+        }
+        public long Number()
+        {
+            return accNo;
+        }
+        public decimal Balance()
+        {
+            return accBal;
+        }
+        public string Type()
+        {
+            return accType.ToString();
+        }
+        private static long NextNumber()
+        {
+            return nextAccNo++;
+        }
+        public decimal Deposit(decimal amount)
+        {
+            accBal += amount;
+            return accBal;
+        }
+        public bool Withdraw(decimal amount)
+        {
+            bool sufficientFunds = accBal >= amount;
+            if (sufficientFunds)
+            {
+                accBal -= amount;
+            }
+            return sufficientFunds;
+        }
+        public void TransferFrom(BankAccount accFrom, decimal amount)
+        {
+            if (accFrom.Withdraw(amount))
+            {
+                this.Deposit(amount);
+            }
+        }
+        static void Main()
+        {
+            BankAccount acc1, acc2, acc3, acc4;
+
+            acc1 = new BankAccount();
+            acc2 = new BankAccount(AccountType.Deposit);
+            acc3 = new BankAccount(100);
+            acc4 = new BankAccount(AccountType.Deposit, 500);
+
+            Write(acc1);
+            Write(acc2);
+            Write(acc3);
+            Write(acc4);
+
+            Console.ReadKey();
+        }
+    }
+}
